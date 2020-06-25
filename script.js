06d1376225121d5da81bf02f8283d055e1f35435
Surajan Shrestha <surajanshrestha35@gmail.com>
2020-06-25 16:55:20 +0545

    Project Added

---
+++ script.js
@@ -0,0 +1,8 @@
+var currentPageTitle=document.getElementsByTagName("title")[0].innerText;
+var sidebarObj=document.getElementsByClassName("sidebar")[0];
+var links=sidebarObj.children;
+for(var i=0;i<sidebarObj.children.length;i++){
+    if(links[i].innerText===currentPageTitle){
+        links[i].className="active";
+    }
+}
